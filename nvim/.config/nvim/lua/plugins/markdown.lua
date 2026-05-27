4b8073f58e284b8878721955fe3e20b1f35bc572
chie <ch1eawa@163.com>
2026-05-27 20:04:53 +0800

    新增markdown增强插件
    - 新增markdown-preview插件，可用浏览器预览markdown，且支持同步滑动
    - 新增aerial插件，可根据treesitter生成大纲

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,89 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
+	-- markdown 终端内预览
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+
+	-- markdown 大纲插件
+	{
+		"stevearc/aerial.nvim",
+		opts = {
+			backends = {
+				"treesitter",
+				"lsp",
+				"markdown",
+				"man",
+			},
+
+			layout = {
+				default_direction = "right",
+				width = 32,
+				min_width = 24,
+			},
+
+			show_guides = true,
+			filter_kind = false,
+		},
+
+		keys = {
+			{
+				"<leader>mo",
+				"<cmd>AerialToggle<cr>",
+				desc = "Markdown Outline",
+			},
+			{
+				"<leader>mn",
+				"<cmd>AerialNext<cr>",
+				desc = "Next Markdown Heading",
+			},
+			{
+				"<leader>mN",
+				"<cmd>AerialPrev<cr>",
+				desc = "Previous Markdown Heading",
+			},
+		},
+		-- Optional dependencies
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+
+	-- markdown 浏览器预览
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = {
+			"MarkdownPreview",
+			"MarkdownPreviewStop",
+			"MarkdownPreviewToggle",
+		},
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_auto_start = 0
+			vim.g.mkdp_auto_close = 1
+			vim.g.mkdp_refresh_slow = 0
+			vim.g.mkdp_command_for_global = 0
+			vim.g.mkdp_open_to_the_world = 0
+			vim.g.mkdp_browser = ""
+			vim.g.mkdp_echo_preview_url = 1
+			vim.g.mkdp_page_title = "${name}"
+		end,
+		keys = {
+			{
+				"<leader>mp",
+				"<cmd>MarkdownPreviewToggle<cr>",
+				desc = "Markdown Preview",
+			},
+		},
+	},
 }
