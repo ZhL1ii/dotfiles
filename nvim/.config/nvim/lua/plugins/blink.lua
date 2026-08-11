58eda380615c4e3fb800fd70000e6abf4a3b32c0
chie <ch1eawa@163.com>
2026-08-11 14:26:18 +0800

    feat(nvim): 新增 lazydev.lua 插件

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -40,7 +40,14 @@ return {
 			},
 
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
+				},
 			},
 
 			signature = {
